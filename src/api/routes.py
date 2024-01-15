a3509972f7a9d3cfe32fa3fec6b2212cdf405018
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2024-01-15 12:38:52 +0000

    Solución de problemas en el código

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,15 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User 
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+
+
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 api = Blueprint('api', __name__)
 
@@ -20,6 +26,28 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/prueba', methods=['POST', 'GET'])
+def handle_hello2():
+
+    response_body = {
+        "message": "prueba"
+    }
+
+    return jsonify(response_body), 200
+
+
+
+
+@api.route('/login', methods=["POST"])
+def login():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+    if username != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=username)
+    return jsonify(access_token=access_token)
 
 
 
--- src/app.py
+++ src/app.py
@@ -64,15 +64,7 @@ def handle_invalid_usage(error):
 
 # generate sitemap with all your endpoints
 
-@app.route("/login", methods=["POST"])
-def login():
-    username = request.json.get("username", None)
-    password = request.json.get("password", None)
-    if username != "test" or password != "test":
-        return jsonify({"msg": "Bad username or password"}), 401
 
-    access_token = create_access_token(identity=username)
-    return jsonify(access_token=access_token)
 
 
 @app.route('/')
